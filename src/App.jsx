8af39ae620fbca406eeff87e23a29b1decbba2f5
YukonWSG <jrsanota@gbox.ncf.edu.ph>
2024-12-17 08:36:24 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./navbar.jsx";
 import Body from "./body.jsx";
-import About from "./About.jsx";
+import About from "./about.jsx";
 import Hobbies from "./hobbies.jsx";
 import Education from "./education.jsx"; // Import the Education component
 import Contact from "./contact.jsx";
